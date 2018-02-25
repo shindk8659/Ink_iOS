8916af7eeb58db77218577d64818cc00c486e6c7
신동규 <shindk8659@naver.com>
2018-02-25 15:49:19 +0900

    add foundation

---
+++ Ink_iOS_Deviloper/VC/LoginVC.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    @IBAction func nextpage(_ sender: Any) {
+        
+        let nextView = self.storyboard!.instantiateViewController(withIdentifier: "navigationVC")
+            self.present(nextView, animated: true)
+        
+        
+    }
+    
+    @IBOutlet weak var Id: UITextField!
+    @IBOutlet weak var Pw: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
